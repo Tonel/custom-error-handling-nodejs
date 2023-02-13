88ccbb22930fb9b4575fef47a0ab67738543311f
Antonello Zanini <antozanini95@gmail.com>
2023-02-13 15:53:01 +0100

    Package.json updated

--- src/appsignal.cjs
+++
@@ -1,7 +0,0 @@
-const { Appsignal } = require("@appsignal/nodejs");
-
-new Appsignal({
-  active: true,
-  name: "My Error Handling Demo App",
-  pushApiKey: "<YOUR_APP_SIGNAL_KEY>",
-});
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 const express = require("express")
 const { greetingRouter } = require("./routes/greetings")
 const { errorHandler } = require("./middlewares/errorHandling")
-const { expressErrorHandler } = require("@appsignal/nodejs")
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
 app.use(greetingRouter)
-
-// register the AppSignal middleware
-app.use(expressErrorHandler)
 
 // register the custom error handling middleware
 app.use(errorHandler)
